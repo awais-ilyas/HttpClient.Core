7b877b63ae0c54646ef6048ed313b177c22a5b6a
Awais Ilyas <im.awaisilyas@gmail.com>
2020-12-29 15:53:30 +0400

    initial commit

---
+++ HttpClientBlazorApp/Models/DayForecastModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpClientBlazorApp.Models
+{
+    public class DayForecastModel
+    {
+        public string Weather_state_name { get; set; }
+        public string Applicable_date { get; set; }
+        public float Min_temp { get; set; }
+        public float Max_temp { get; set; }
+    }
+}
